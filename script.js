12331cfebc8c65aaf74e68e7eb1883a6924a338f
VadymL <110418620+GramenCeleritas@users.noreply.github.com>
2023-03-10 05:03:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
-
-
 let num1 = parseFloat(prompt("Enter the first number:"));
 let num2 = parseFloat(prompt("Enter the second number:"));
-
 
 alert(`Sum: ${num1 + num2}\nDifference: ${num1 - num2}\nMultiplication: ${num1 * num2}\nDivision: ${num1 / num2}`);
