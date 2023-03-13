576764e28516fc57f81646ef425bed75f2bd6623
admin <vadim771@ukr.net>
2023-03-13 23:48:35 +0200

    remodeled

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
 
-var num1 = prompt("Enter the first number:");
-var num2 = prompt("Enter the second number:");
+if (num1 === "" || num2 === "") {
+  alert("Ups, looks like one of the feelds are empty");
+} else {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
 
-var sum = Number(num1) + Number(num2);
-var difference = num1 - num2;
-var multiplication = num1 * num2;
-var division = num1 / num2;
-
-alert("Sum: " + sum + "\nDifference: " + difference + "\nMultiplication: " + multiplication + "\nDivision: " + division);
+  if (num2 === 0) {
+    alert("Error: Cannot divide by zero");
+  } else if (num1 < num2) {
+    const confirmOperation = confirm("Are you sure you want to continue the operation, the second number is bigger than first one?");
+    if (confirmOperation) {
+      alert(`Difference: ${num1 - num2}`);
+      
+    }
+  } else if (num1 >= num2) {
+    alert(`Sum: ${num1 + num2}\nDifference: ${num1 - num2}\nMultiplication: ${num1 * num2}\nDivision: ${num1 / num2}`);
+  }
+}
